8074bd1c34293e625f59dc76bc439cdb12b10d79
Thefirstxerg <thefirstxerg@gmail.com>
2025-11-11 12:29:25 +0200

    Remove react-snap and update app entry point
    Removed react-snap from devDependencies and build script in portfolio/package.json, along with related configuration. Updated index.js to use React.StrictMode and simplified rendering logic. This streamlines the build process and modernizes the app entry point.

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -7,11 +7,11 @@ import reportWebVitals from './reportWebVitals';
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
-if (rootElement.hasChildNodes()) {
-  root.hydrate(<App />);
-} else {
-  root.render(<App />);
-}
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
